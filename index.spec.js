44f7c15c182897a2e306253ce2e85a16b866ceaf
Youssef Jerbi <youssefjerbi@insat.u-carthage.tn>
2022-04-29 14:04:03 +0100

    fix test

--- index.spec.js
+++ index.spec.js
@@ -2,7 +2,7 @@ const {helloWorld} = require('./index')
 // hello world test
 describe('hello world', function() {
     it('should return hello world', function() {
-        expect(helloWorld()).toEqual('Hello World');
+        expect(helloWorld()).toEqual('Hello World!');
     });
     }
 );
